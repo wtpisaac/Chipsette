95937fce33f639ca66052503946b89e67a15d584
Isaac Trimble-Pederson <wtpisaac@gmail.com>
2022-10-03 18:39:39 -0700

    Swap to programmatic instantiation

--- Chipsette/SceneDelegate.swift
+++ Chipsette/SceneDelegate.swift
@@ -16,7 +16,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let foundScene = (scene as? UIWindowScene) else { return }
+        
+        // Instantiate initial window and view controller
+        let window = UIWindow(windowScene: foundScene)
+        
+        let vc = ViewController()
+        window.rootViewController = vc
+        
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- Chipsette/ViewController.swift
+++ Chipsette/ViewController.swift
@@ -12,6 +12,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        let safeArea = view.safeAreaLayoutGuide
+        
+        // Add label
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello, World!"
+        label.textAlignment = .center
+        view.addSubview(label)
+        
+        label.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
+        label.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor).isActive = true
+        
+        
     }
 
 
